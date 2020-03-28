cc39417ff5f8f2808fd90f5109f352056ba1ffd4
kampfknoedel <lukas.kuder@web.de>
2020-03-28 09:20:30 +0100

    test

--- ei-o-mat/mainwindow.cpp
+++ ei-o-mat/mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPushButton>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setFixedSize(500,500);
+
+    QPushButton *neuerknopf = new QPushButton("Test", this);
+    neuerknopf->setGeometry(20, 120, 50, 30);
+    connect(neuerknopf, SIGNAL(clicked()), qApp, SLOT(quit()));
+
 }
 
 MainWindow::~MainWindow()
